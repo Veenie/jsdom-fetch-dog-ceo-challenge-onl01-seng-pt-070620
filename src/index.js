bbdebe971fe30236729fcac03b2a919079fded17
Raveen K Sharma <raveen08@gmail.com>
2021-01-20 22:28:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ function renderImg(images) {
   console.log("images", images)
   
   const contain = document.querySelector('dog-image-container')
-  images.message.forEach(element => console.log(element))
-  //=>{
-  //let newImage = document.createElement('img');
+  images.message.forEach(element => 
+  let newImage = document.createElement('img');
   //newImage.innerHTML = `<img src="${img}">`
   //contain.appendChild(newImage);
-  
+ ) 
 }
 
 
