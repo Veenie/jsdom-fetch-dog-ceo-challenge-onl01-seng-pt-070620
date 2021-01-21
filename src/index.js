c579d078cce65c241b867559682abfd219dd4713
Raveen K Sharma <raveen08@gmail.com>
2021-01-21 00:29:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ function fetchBreed() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   return fetch(breedUrl)
   .then(resp => resp.json())
-  .then(json => renderBreed(json.message));
+  .then(json => renderBreed(json));
 }
 
 function renderBreed(breed) {
   const contain = document.getElementById('dog-breeds');
-  breed.forEach(function(r){console.log(r)})
-  contain.innerHTML = breed
+  
+  breed.message.forEach()
   console.log(breed)
 }
 
