8c1f27539de9d1d9ea5c4b74fd49f738266d744b
Raveen K Sharma <raveen08@gmail.com>
2021-01-21 00:24:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function fetchBreed() {
 
 function renderBreed(breed) {
   const contain = document.getElementById('dog-breeds');
-  breed.forEach
+  breed.forEach(function)
   contain.innerHTML = breed
   console.log(breed)
 }
